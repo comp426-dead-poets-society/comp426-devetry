b69ac98cda12b57a25f824cbd62ab7cc9a87154f
Malik Jabati <malikjabati@maliks-mbp.wireless-1x.unc.edu>
2019-12-10 18:11:59 -0500

    Add very basic rendering to PostPage

--- devetry/src/pages/PostPage.js
+++ devetry/src/pages/PostPage.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
+import {getPoem} from '../api/publicAPI';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import '../App.scss';
+import Loading from "../components/Loading"
+import { getStatus } from "../api/accountAPI"
+import Header from '../components/Header'
+import MainPage from './MainPage'
+import UserPage from './UserPage'
+import SearchPage from './SearchPage'
+import NotFound from './NotFound'
+import SubmitPage from './SubmitPage'
 
 export default class PostPage extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            poemId: undefined,
+            title: '',
+            body: '',
+            author: '',
+            createdAt: undefined,
+            updatedAt: undefined,
+            likeCount: 0
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ poemId: this.props.match.params.id});
+        try {
+            //TODO: Set poem state with values from grabbed poem
+            //poem = getPoem(this.props.match.params.id);
+        } catch (error) {
+            return <div> 404. Poem does not exist!</div>
+        }
+        
+
+    }
+
     render() {
         return (
             <div>
-                
+                <p>{this.state.poemId}</p>
             </div>
         )
     }
