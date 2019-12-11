5c789b3c566002f7dd66e6941b5c4a335d9af668
Sam Lowe <sam@slowe.io>
2019-12-11 18:27:08 -0500

    extra user data

--- devetry/src/components/CreateAccount.js
+++ devetry/src/components/CreateAccount.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {createAccount} from "../api/accountAPI";
+import React, {useState} from 'react';
+import { createAccount } from "../api/accountAPI";
 
 function CreateAccount() {
   return (
@@ -9,15 +9,49 @@ function CreateAccount() {
         e.preventDefault();
         const name = e.target.name.value;
         const pass = e.target.pass.value;
-        await createAccount({name, pass});
+        let color;
+        switch(e.target.color.selectedIndex) {
+          case 0:
+            color = "#BA55D3"
+            break;
+          case 1:
+            color = "#B0C4DE";
+            break;
+          case 2:
+            color = "#778899"
+            break;
+          case 4:
+            color = "#FF8C00"
+            break;
+          case 3:
+            color = "#F08080"
+            break;          
+        }
+        let data = { "name":name, "pass":pass, "data":{"color":color} }
+        await createAccount(data);
+        window.location.reload();
       }}>
         <div className="field">
-          <input className="input" placeholder="Username" type="text" name="name"/>
+          <input className="input" placeholder="Username" type="text" name="name" />
         </div>
         <div className="field">
-          <input className="input" placeholder="Password" type="password" name="pass"/>
+          <input className="input" placeholder="Password" type="password" name="pass" />
         </div>
-        <input className="button is-primary" type="submit" value={"Create"}/>
+        <div className="field">
+          <label class="label">Preferred Color</label>
+          <div class="control">
+            <div class="select">
+              <select name="color">
+                <option value="1">Purple</option>
+                <option value="2">Blue</option>
+                <option value="3">Gray</option>
+                <option value="4">Pink</option>
+                <option value="5">Orange</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <input className="button is-primary" type="submit" value={"Create"} />
       </form>
     </div>
   );
--- devetry/src/config/Axios.js
+++ devetry/src/config/Axios.js
@@ -5,7 +5,7 @@ export const baseURL = 'http://localhost:3001';
 
 export const getAxiosInstance = function (middlePath = '') {
   return axios.create({
-    headers: {Authorization: `Bearer ${getToken()}`},
+    headers: {"Authorization": `Bearer ${getToken()}`},
     baseURL: `${baseURL}${middlePath}`
   });
 };
