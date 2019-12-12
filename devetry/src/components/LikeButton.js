d8c0697f78f0450b9c1636f65dcec33e047fd7eb
Bridget Mizener <westsidebballer34@gmail.com>
2019-12-11 23:15:04 -0500

    dynamically updates the like button

--- devetry/src/components/LikeButton.js
+++ devetry/src/components/LikeButton.js
@@ -3,16 +3,35 @@ import '../App.scss';
 import { likePoem, getNumLikes } from '../api/publicAPI';
 
 export class LikeButton extends Component {
+    state = {
+        count: this.props.likeCount
+    }
 
+    async componentDidMount() {
+        let likes;
+        try {
+            await likePoem(this.props.poemId);
+        }
+        catch(error){
+            console.log("Uh oh.");
+        }
+    }
+
+    handleClick = () => {
+        this.setState((prevState, { count }) => ({
+          count: prevState.count + 1
+        }));
+    }
 
     //TODO: Need to find a way to dynamically update likes on the page
     render() {
+        let likes = this.state.count;
         return (
             <React.Fragment>
                 <div>
                     <p>
-                        <button onClick={ async () => await likePoem(this.props.poemId) } className='likeButton'
-                        type='button'>Like!</button> {this.props.likeCount}
+                        <button onClick={ this.handleClick } className='likeButton'
+                        type='button'>Like!</button> {this.state.count}
                     </p>
                 </div>
             </React.Fragment>
